0a96430543d44d56294b55601c51d8bfe74ba466
Vraj Patel <114293277+AshenWitch96@users.noreply.github.com>
2025-08-28 11:25:49 +0530

    Update Problem.java

--- Problem.java
+++ Problem.java
@@ -2,29 +2,39 @@ import java.io.FileReader;
 import java.util.*;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import java.math.BigInteger;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Problem {
-    public static int decode(int base, String val) {
-        return Integer.parseInt(val, base);
+
+    public static BigInteger decodeBig(String val, int base) {
+        return new BigInteger(val, base);
     }
 
-    public static double interpolate(List<double[]> pts) {
+    public static BigDecimal interpolate(List<BigInteger[]> pts) {
         int n = pts.size();
-        double constant = 0;
+        BigDecimal constant = BigDecimal.ZERO;
+
         for (int i = 0; i < n; i++) {
-            double xi = pts.get(i)[0];
-            double yi = pts.get(i)[1];
-            double li = 1;
+            BigDecimal xi = new BigDecimal(pts.get(i)[0]);
+            BigDecimal yi = new BigDecimal(pts.get(i)[1]);
+            BigDecimal li = BigDecimal.ONE;
+
             for (int j = 0; j < n; j++) {
                 if (i != j) {
-                    double xj = pts.get(j)[0];
-                    li *= (0 - xj) / (xi - xj);
+                    BigDecimal xj = new BigDecimal(pts.get(j)[0]);
+                    li = li.multiply(BigDecimal.ZERO.subtract(xj)
+                            .divide(xi.subtract(xj), 50, RoundingMode.HALF_UP));
                 }
             }
-            constant += yi * li;
+
+            constant = constant.add(yi.multiply(li));
         }
+
         return constant;
     }
+
     public static void main(String[] args) {
         try {
             JSONParser parser = new JSONParser();
@@ -32,19 +42,26 @@ public class Problem {
             JSONObject keys = (JSONObject) input.get("keys");
             long n = (long) keys.get("n");
             long k = (long) keys.get("k");
-            List<double[]> pts = new ArrayList<>();
+
+            List<BigInteger[]> pts = new ArrayList<>();
+
             for (Object key : input.keySet()) {
                 String kStr = (String) key;
-                if (kStr.equals("keys")) continue; // skip "keys" object
-                long x = Long.parseLong(kStr);
+                if (kStr.equals("keys")) continue;
+
+                BigInteger x = new BigInteger(kStr);
                 JSONObject yObj = (JSONObject) input.get(kStr);
                 int base = Integer.parseInt((String) yObj.get("base"));
                 String val = (String) yObj.get("value");
-                int y = decode(base, val);
-                pts.add(new double[]{x, y});
+
+                BigInteger y = decodeBig(val, base);
+                pts.add(new BigInteger[]{x, y});
             }
-            double c = interpolate(pts);
-            System.out.println("Constant (c) is " + Math.round(c));
+
+            BigDecimal c = interpolate(pts);
+
+            System.out.println("Secret constant (c): " + c.toPlainString());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
